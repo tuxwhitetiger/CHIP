f26757f2a0a56eb985fa23d74dc7ddeb03ddfec0
tux <tuxwhitetiger@gmail.com>
2022-06-23 19:56:08 +0100

    commit on 'master'

---
+++ server.py
@@ -0,0 +1,44 @@
+import socket
+from _thread import *
+
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+face = "Happy face"
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind((HOST, PORT))
+s.settimeout(0.2)
+s.listen()
+
+socketList = []
+
+socketList.append(s)
+
+while True:
+    ##roll though list of avalible sockets
+    for sock in socketList:
+        ##if looking at server see if we can pickup another client
+        if sock == s:
+            try:
+                Client,addr = s.accept()
+                Client.settimeout(0.2)
+                socketList.append(Client)
+            except socket.timeout:
+                pass
+        ##if not looking at server socket deal with any client request to get set face    
+        else:
+            try:
+                data = sock.recv(2048)
+                info = data.decode('utf-8')
+                if info:
+                    if "Get Face" in info:
+                        sock.sendall(face.encode())
+                    elif "Set Face:" in info:
+                        face = info.split(':',1)[1]
+                        sock.sendall("done".encode())
+                    else:
+                        print("could not deal with:",info)
+            except socket.timeout:
+                pass
+
