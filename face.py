564494ba10c4421c7ffdbe3a66c43ebfd38426cd
tux <tuxwhitetiger@gmail.com>
2022-06-27 00:12:55 +0100

    commit on 'master'

--- face.py
+++ face.py
@@ -3,6 +3,21 @@
 from PIL import Image
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
 
+
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+network = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+network.connect((HOST, PORT))
+
+def networksendGetface():
+    network.sendall(data.encode())
+    data = network.recv(1024)
+    return data.decode('utf-8')
+
+
+
+
 # Configuration for the matrix
 options = RGBMatrixOptions()
 options.rows = 32
@@ -15,7 +30,25 @@ options.hardware_mapping = 'regular'
 matrix = RGBMatrix(options = options)
 
 image = Image.open("./faces/test.gif")
+
 while True:
+    face = networksendGetface()
+
+    if "SAD FACE" in face:
+        image = Image.open("./faces/sad.gif")
+    elif "HAPPY FACE" in face:
+        image = Image.open("./faces/happy.gif")
+    elif "FLAG FACE" in face:
+        image = Image.open("./faces/flag.gif")
+    elif "ANGRY FACE" in face:
+        image = Image.open("./faces/angry.gif")
+    elif "GIF FACE" in face:
+        image = Image.open("./faces/pacman.gif")
+    elif "OH FACE" in face:
+        image = Image.open("./faces/oh.gif")
+    elif "WHAT FACE" in face:
+        image = Image.open("./faces/what.gif")
+
     matrix.SetImage(image.convert('RGB'))
 
 
