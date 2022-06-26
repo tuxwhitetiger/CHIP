c71a8575fd31ca0d333087deeb55628ea06130c1
tux <tuxwhitetiger@gmail.com>
2022-06-26 16:22:17 +0100

    commit on 'master'

---
+++ face.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+from PIL import Image
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
+
+# Configuration for the matrix
+options = RGBMatrixOptions()
+options.rows = 32
+options.cols = 64
+options.chain_length = 1
+options.parallel = 1
+options.gpio_slowdown = 3
+options.hardware_mapping = 'regular'
+
+matrix = RGBMatrix(options = options)
+
+image = Image.open("./faces/test.gif")
+matrix.SetImage(image.convert('RGB'))
+
+
+
+
+
