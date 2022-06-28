d23c207bd01af66ffb3cae656a3c989ab13e4a6b
tux <tuxwhitetiger@gmail.com>
2022-06-28 20:37:26 +0100

    commit on 'master'

--- face.py
+++ face.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 import socket
 from PIL import Image
-from rgbmatrix import RGBMatrix, RGBMatrixOptions
+from PIL import GifImagePlugin
+import random
+from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
+import time
 
 
 
@@ -29,8 +32,63 @@ options.gpio_slowdown = 3
 options.hardware_mapping = 'regular'
 
 matrix = RGBMatrix(options = options)
-
+face = networksendGetface()
 image = Image.open("./faces/test.gif")
+
+
+def playAnimatedGif(fileName):
+    image = Image.open(fileName)
+    for frame in range(0,image.n_frames):
+        if frame % 10 == 0:
+            checkface = networksendGetface()
+            if checkface not in face:
+                face = checkface
+                break
+        
+        image.seek(frame)
+        matrix.SetImage(image.convert('RGB'))
+
+
+def showStaticGif(fileName):
+    image = Image.open(fileName)
+    matrix.SetImage(image.convert('RGB'))
+
+def showWhatFace():
+    ## do the ? spam in here 
+    #set up vars
+    offscreen_canvas = matrix.CreateFrameCanvas()
+    font1 = graphics.Font()
+    font1.LoadFont("./fonts/5x8.bdf")   
+    font2 = graphics.Font()
+    font2.LoadFont("./fonts/6x10.bdf")
+    font3 = graphics.Font()
+    font3.LoadFont("./fonts/7x13.bdf")
+    font4 = graphics.Font()
+    font4.LoadFont("./fonts/9x15.bdf")
+    mfonts = [font1,font2,font3,font4]
+    i = 0
+    count = 50
+    sleepTimer = 2.0
+    #need to run each time ? placed
+    while i<count:
+        textColor =  graphics.Color(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) #random color
+        font = random.choice(mfonts)
+        x = random.randint(0, 120)
+        y = random.randint(0, 30)
+        graphics.DrawText(offscreen_canvas, font, x, y, textColor, "?")
+        sleepTimer = sleepTimer-(sleepTimer/(count-i))
+        time.sleep(sleepTimer)
+
+
+
+def showHappyFace():
+    ## random bounce between with in max and chance of double blink face /// animate it?
+    matrix.SetImage(image.convert('RGB'))
+
+def showAngryFace():
+    ## growl + nose flareing
+    matrix.SetImage(image.convert('RGB'))
+
 
 while True:
     face = networksendGetface()
@@ -40,11 +98,11 @@ while True:
     elif "Happy face" in face:
         image = Image.open("./faces/happy.gif")
     elif "Flag face" in face:
-        image = Image.open("./faces/flag.gif")
+        showStaticGif("./faces/flag.gif")
     elif "Angry face" in face:
         image = Image.open("./faces/angry.gif")
     elif "Gif face" in face:
-        image = Image.open("./faces/pacman.gif")
+        playAnimatedGif("./faces/pacman.gif")
     elif "Oh face" in face:
         image = Image.open("./faces/oh.gif")
     elif "What face" in face:
