8b88b8a8eee898ae37c1a3eefcd0a5a731f75e25
tux <tuxwhitetiger@gmail.com>
2022-06-29 18:26:25 +0100

    .

--- face.py
+++ face.py
@@ -45,7 +45,7 @@ def playAnimatedGif(fileName):
             if checkface not in face:
                 face = checkface
                 break
-        Time.sleep(0.4)
+        time.sleep(0.4)
         image.seek(frame)
         matrix.SetImage(image.convert('RGB'))
 
