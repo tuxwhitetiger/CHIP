d13308f8d76e39216d17de31fcc8795b23777f23
tux <tuxwhitetiger@gmail.com>
2022-06-30 22:44:34 +0100

    ,

--- face.py
+++ face.py
@@ -19,9 +19,6 @@ def networksendGetface():
     print(data.decode('utf-8'))
     return data.decode('utf-8')
 
-
-
-
 # Configuration for the matrix
 options = RGBMatrixOptions()
 options.rows = 32
@@ -30,11 +27,9 @@ options.chain_length = 1
 options.parallel = 1
 options.gpio_slowdown = 3
 options.hardware_mapping = 'regular'
-
 matrix = RGBMatrix(options = options)
 face = networksendGetface()
 image = Image.open("./faces/test.gif")
-
 
 def playAnimatedGif(fileName):
     image = Image.open(fileName)
@@ -49,14 +44,11 @@ def playAnimatedGif(fileName):
         image.seek(frame)
         matrix.SetImage(image.convert('RGB'))
 
-
 def showStaticGif(fileName):
     image = Image.open(fileName)
     matrix.SetImage(image.convert('RGB'))
 
-
 def showSadFace():
-    ## random bounce between with in max and chance of double blink face /// animate it?
     image = Image.open("./faces/sad.gif")
     matrix.SetImage(image.convert('RGB'))
 
@@ -65,9 +57,7 @@ def showHappyFace():
     image = Image.open("./faces/happy.gif")
     matrix.SetImage(image.convert('RGB'))
 
-
 def showFlagFace():
-    ## random bounce between with in max and chance of double blink face /// animate it?
     showStaticGif("./faces/flag.gif")
 
 def showAngryFace():
@@ -76,18 +66,15 @@ def showAngryFace():
     matrix.SetImage(image.convert('RGB'))
 
 def showGifFace():
-    ## growl + nose flareing
     playAnimatedGif("./faces/pacman.gif")
 
 def showOhFace():
-    ## growl + nose flareing
     image = Image.open("./faces/oh.gif")
     matrix.SetImage(image.convert('RGB'))
 
 def showWhatFace():
     ## do the ? spam in here 
     #set up vars
-
     offscreen_canvas = matrix.CreateFrameCanvas()
     offscreen_canvas.Clear()
     font1 = graphics.Font()
@@ -106,22 +93,21 @@ def showWhatFace():
     while i<count:
         textColor =  graphics.Color(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) #random color
         font = random.choice(mfonts)
-        x = random.randint(0, 120)
-        y = random.randint(0, 30)
+        x = random.randint(0, 128)
+        y = random.randint(0, 32)
         graphics.DrawText(offscreen_canvas, font, x, y, textColor, "?")
         matrix.SwapOnVSync(offscreen_canvas)
         sleepTimer = sleepTimer-(sleepTimer/(count-i))
         i = i + 1 
         time.sleep(sleepTimer)
         time.sleep(0.001)
-
     time.sleep(0.5)
 
 
 
 while True:
     face = networksendGetface()
-    print(face)
+    
     if "Sad face" in face:
         showSadFace()
     elif "Happy face" in face:
