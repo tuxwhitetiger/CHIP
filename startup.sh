065ba2a22aec609c6d661b240854aee006a3a0bc
tux <tuxwhitetiger@gmail.com>
2022-07-10 22:12:41 +0100

    .

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
-sudo python /home/tux/CHIP/server.py
-sudo python /home/tux/CHIP/face.py
-python /home/tux/CHIP/voice2.py
+#!/bin/bash
+sudo python /home/tux/CHIP/server.py &
+sudo python /home/tux/CHIP/face.py &
+python /home/tux/CHIP/voice2.py &
