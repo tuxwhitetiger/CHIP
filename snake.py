521ac1ec775d261d9f59197cceedf9174830f87e
tux <tuxwhitetiger@gmail.com>
2022-07-13 17:34:34 +0100

    .

--- snake.py
+++ snake.py
@@ -1,6 +1,6 @@
 from array import *
 from enum import Enum
-#from random import random
+from random import random
 
 
 class Direction(Enum):
@@ -34,6 +34,7 @@ class aSnake :
     head = Head()
     segments = []
     food = Food()
+    global random
 
     def makenewfood():
         global food
