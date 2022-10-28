f8f0ec5cbbf56a2ec36a180ca3ef5b4d25976cf2
tux <tuxwhitetiger@gmail.com>
2022-10-28 22:02:14 +0100

    .

--- startup.sh
+++ startup.sh
@@ -4,8 +4,12 @@ git -C /home/tux/CHIP3 pull https://github.com/tuxwhitetiger/CHIP3.0.git
 cd /home/tux/CHIP3/CHIP 
 sudo mcs -out:bob.exe *.cs /r:RGBLedMatrix.dll
 cd /home/tux/CHIP
-sudo python /home/tux/CHIP/server.py &
-sudo python /home/tux/CHIP/face.py &
-python /home/tux/CHIP/voice2.py &
-
-#this  is a test
+sleep 15
+sudo python server.py &
+sleep 15
+cd /home/tux/CHIP
+su tux -c "python3 voice2.py &"
+sleep 15
+cd /home/tux/CHIP3/CHIP
+sudo mono bob.exe &
+sudo systemctl start isc-dhcp-server.service
