59724bab62f2afccd6cedb5d6a6f89b75e44f0bf
tuxwhitetiger <tuxwhitetiger@gmail.com>
2023-03-03 17:32:33 +0000

    ..

--- startup.sh
+++ startup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 sleep 30
-git -C /home/tux/CHIP pull https://github.com/tuxwhitetiger/CHIP.git
-git -C /home/tux/CHIP3 pull https://github.com/tuxwhitetiger/CHIP3.0.git
+if ping -c 1 1.1.1.1 &> /dev/null
+then
+  git -C /home/tux/CHIP pull https://github.com/tuxwhitetiger/CHIP.git
+  git -C /home/tux/CHIP3 pull https://github.com/tuxwhitetiger/CHIP3.0.git
+else
+  echo "no internet"
+fi
 cd /home/tux/CHIP3/CHIP 
 sudo mcs -out:bob.exe *.cs /r:RGBLedMatrix.dll
 cd /home/tux/CHIP
@@ -12,7 +17,7 @@ cd /home/tux/CHIP
 su tux -c "python3 voice2.py &"
 sleep 15
 cd /home/tux/CHIP3/CHIP
-#sudo mono bob.exe &
+sudo systemctl start isc-dhcp-server.service
 cd /home/tux/CHIP3/CHIP/Debug/netcoreapp3.1
 sudo dotnet CHIP.dll
-sudo systemctl start isc-dhcp-server.service
+
