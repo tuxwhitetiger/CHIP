d6755d488b07622ae492dd2a9eef5538e42e6d2c
tuxwhitetiger <tuxwhitetiger@gmail.com>
2023-10-08 20:43:37 +0100

    ..

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@ sleep 30
 if ping -c 1 1.1.1.1 &> /dev/null
 then
   git -C /home/tux/CHIP pull https://github.com/tuxwhitetiger/CHIP.git
-  git -C /home/tux/CHIP3 pull https://github.com/tuxwhitetiger/CHIP3.0.git
+  git -C /home/tux/CHIP3/CHIP3.0 pull https://github.com/tuxwhitetiger/CHIP3.0.git
 else
   echo "no internet"
 fi
